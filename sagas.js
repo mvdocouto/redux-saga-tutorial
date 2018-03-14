ef1741e79222744ba907aff82dc3c7a9d5705657
Marcus Couto <marcus.couto@m4u.com.br>
2018-03-14 19:29:08 -0300

    Ajuste nas sagas

--- sagas.js
+++ sagas.js
@@ -1,12 +1,12 @@
 import { delay } from 'redux-saga'
-import { put, takeEvery, all} from 'redux-saga/effects'
+import { put, takeEvery, all, call} from 'redux-saga/effects'
 
 export function* helloSaga(){
     console.log("hello....");
 }
 
-function* incrementAsync(){
-    yield delay(1000);
+export function* incrementAsync(){    
+    yield call(delay, 1000);
     yield put({type: 'INCREMENT'});
 }
 
