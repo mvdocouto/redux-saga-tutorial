dd3f2b8ec329d70de5fc2f1a23bb5e6d67392a07
Marcus Couto <marcus.couto@m4u.com.br>
2018-03-14 18:35:16 -0300

    exportando as multiplas sagas.

--- sagas.js
+++ sagas.js
@@ -1,15 +1,23 @@
 import { delay } from 'redux-saga'
-import { put, takeEvery} from 'redux-saga/effects'
+import { put, takeEvery, all} from 'redux-saga/effects'
 
 export function* helloSaga(){
     console.log("hello....");
 }
 
-export function* incrementAsync(){
+function* incrementAsync(){
     yield delay(1000);
     yield put({type: 'INCREMENT'});
 }
 
-export function* watchIncrementAsync(){ 
+function* watchIncrementAsync(){
     yield takeEvery('INCREMENT_ASYNC', incrementAsync);
+}
+
+
+export default function* rootSaga(){
+    yield all([
+        helloSaga(),
+        watchIncrementAsync()
+    ])
 }
